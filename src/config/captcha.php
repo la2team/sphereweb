98a268ac2897c077d199e8c4e6fc9a877c1b225f
scantraxx2222 <153460208+scantraxx2222@users.noreply.github.com>
2024-04-14 00:36:31 +0300

    Reapply "Update captcha.php"

--- src/config/captcha.php
+++ src/config/captcha.php
@@ -4,7 +4,7 @@
 // There are two of them either Google or Default
 // if you decide to use Google Recaptcha v3
 // Google Kapchu Keys Take here - https://www.google.com/recaptchas/about/ in the section "V3 Admin Console"
-const CAPTCHA = "google";
+const CAPTCHA = "default";
 
 // for Google Kapchi
 const GOOGLE_CLIENT_KEY = "";
