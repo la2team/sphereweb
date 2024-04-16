7b480a3db42d9757b2eb1f3f710f5221f4eddd6c
scantraxx2222 <153460208+scantraxx2222@users.noreply.github.com>
2024-04-16 21:18:50 +0300

    Update referral.php

--- src/config/referral.php
+++ src/config/referral.php
@@ -8,8 +8,8 @@ const PK = 1;
 //How many bonuses to give
 const DONATE_BONUS_AMOUNT = 20;
 
-// If a person has brought the players who registered by his referral link.
+// If a person entered the players who registered at his referral link.
 // Should Sphere-Coin give the leader if the user he received replenishes his balance?
 const REFERRAL_LEADER_BONUS_ENABLE = true;
-/
+// What percentage of sphere-Coin replenishment to give the referral leader
 const REFERRAL_LEADER_BONUS_VALUE = 10;
